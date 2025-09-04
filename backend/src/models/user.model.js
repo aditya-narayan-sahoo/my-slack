165890ce396fa91385ab3cefb86aff3611859d5f
Aditya Narayan Sahoo <aditysahoo246@gmail.com>
2025-09-04 08:14:21 +0530

    Auth implementation with inngest, clerk

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model("User", userSchema);
